255009e3b38011986671968d913a5f8db9fd3dee
Anechua <98209192+Anechua@users.noreply.github.com>
2022-07-14 23:17:16 +0300

    Вносит корректировки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 //Генератор случайных целых чисел из переданного диапазона
 const getRandomInt = (a, b) => {
-    const min = Math.min(a, b);
-    const max = Math.max(a, b);
-    const result = Math.floor(Math.random() * (max - min)) + min;
-    return Math.abs(result);
-}
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  const result = Math.floor(Math.random() * (max - min)) + min;
+  return Math.abs(result);
+};
 
 //Функция для проверки максимальной длины строки
-const checkStringLength = (string, maxLength) => {
-    return (string.length <= maxLength) ? true : false;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-getRandomInt();
-checkStringLength();
+getRandomInt(9, 0);
+checkStringLength('sdfgfgj', 6);
