92a8fac93e4c568684cb16eca5171ed0ac65926a
Anechua <98209192+Anechua@users.noreply.github.com>
2022-07-14 22:53:29 +0300

    Добавляет вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Генератор случайных целых чисел из переданного диапазона
+const getRandomInt = (a, b) => {
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    const result = Math.floor(Math.random() * (max - min)) + min;
+    return Math.abs(result);
+}
+
+//Функция для проверки максимальной длины строки
+const checkStringLength = (string, maxLength) => {
+    return (string.length <= maxLength) ? true : false;
+}
+
+getRandomInt();
+checkStringLength();
